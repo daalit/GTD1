feb74bacd7fa0c77ab3704e6f32703e57afab561
Monika-feg <mala88@hotmail.comcd>
2024-09-27 10:17:49 +0200

    gjort en class till GUI

---
+++ GTD/src/GUI.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI {
+
+    // här vill vi ha all vår GUI kod.
+    JFrame frame = new JFrame("Test GUI");
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel();
+    // panelen ska ha ett utseende
+
+    
+
+   // frame.add(panel);
+
+}
