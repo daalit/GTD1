46252be2e641409f9398eaf6a46cec7ff9bcc4eb
Niklasgth <Niklasgth@hotmail.com>
2024-09-27 09:59:03 +0200

    Startup

--- GTD/src/App.java
+++ GTD/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        System.out.println("Statup messege");
+
+
     }
 }
+
+
+// vi ska skapa en "att göra lista"
+
+//funktion för lägga till inlägg
+
+//funktion för markera som klar
+
+//funktion för ta bort inlägg
+
+//gui grunder för programmet
